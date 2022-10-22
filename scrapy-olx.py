ca2a6cbc82ad29c9accfea4f699483cf63190a3d
joabrito <joabrito@uolinc.com>
2022-10-22 10:48:36 -0300

    get all ads from url until last page

--- scrapy-olx.py
+++ scrapy-olx.py
@@ -24,9 +24,33 @@ def count_pages(url: str) -> int:
     return last_page
 
 
+def get_all_ads(url: str, last_page: int) -> list:
+    ad_lists = []
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42",
+    }
+    params = {
+        "pe": "1200",
+        "sp": "2",
+    }
+
+    for i in range(1, last_page+1):
+        params["o"] = i
+
+        res = requests.get(url, headers=headers, params=params)
+        print(res.request.url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        ad_lists.append(soup.find(id='ad-list'))
+
+    return ad_lists
+
+
 def main():
     url = 'https://rj.olx.com.br/norte-do-estado-do-rio/regiao-dos-lagos/cabo-frio/imoveis/aluguel?'
     page = count_pages(url)
+
+    ads = get_all_ads(url, page)
 
 
 main()
